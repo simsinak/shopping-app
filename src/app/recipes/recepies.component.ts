9df3f4776c0c093b227b2b992b108fdb13257feb
sinaaskarnejad <sina.askarnejad@gmail.com>
2025-01-02 00:38:06 +0330

    [enhancement]build components structures

---
+++ src/app/recipes/recepies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recepies',
+  standalone: false,
+  
+  templateUrl: './recepies.component.html',
+  styleUrl: './recepies.component.css'
+})
+export class RecepiesComponent {
+
+}
---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-edit',
+  standalone: false,
+  
+  templateUrl: './shopping-edit.component.html',
+  styleUrl: './shopping-edit.component.css'
+})
+export class ShoppingEditComponent {
+
+}
