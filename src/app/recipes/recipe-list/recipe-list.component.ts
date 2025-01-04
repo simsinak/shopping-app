5a5c724aa3355fb21f7ac778821b101306dab959
sinaaskarnejad <sina.askarnejad@gmail.com>
2025-01-04 23:08:07 +0330

    [enhancement]make the recipe list and detail better

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   standalone: false,
-  
+
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+     recipes: Recipe[] = [
+       new Recipe("Delicious",
+         "blah blah blah",
+         "https://www.spendwithpennies.com/wp-content/uploads/2022/12/1200-The-Best-Meatloaf-Recipe-SpendWithPennies.jpg")
+         ,
+       new Recipe("Delicious",
+         "blah blah blah",
+         "https://www.spendwithpennies.com/wp-content/uploads/2022/12/1200-The-Best-Meatloaf-Recipe-SpendWithPennies.jpg")
+     ]
 }
